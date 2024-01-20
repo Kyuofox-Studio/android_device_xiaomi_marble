946199edfa83e4ad22d11990d0b842531c3ab42e
Arian <arian.kulmer@web.de>
2024-01-20 11:44:28 +0100

    marble: Include common framework compatibility matrices
    Change-Id: Ibb7838384bb448c70c8e5a12ab5ca3a2a0868588

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,11 @@ TARGET_USERIMAGES_USE_F2FS := true
 # VINTF
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
