151217bdb2f09e0b93d4a8c8e5ce8ebd2d63a9f8
Arian <arian.kulmer@web.de>
2024-01-20 11:41:55 +0100

    marble: vintf: Add xiaomi manifest additions
    Change-Id: I5954f5279de6a0fc85d846cf75e38ed366b7b948

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,9 +118,12 @@ TARGET_USERIMAGES_USE_F2FS := true
 # VINTF
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
 
-DEVICE_MANIFEST_UKEE_FILES := $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MANIFEST_UKEE_FILES := \
+    $(DEVICE_PATH)/configs/vintf/manifest.xml \
+    $(DEVICE_PATH)/configs/vintf/manifest_xiaomi.xml
 
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/configs/vintf/xiaomi_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
