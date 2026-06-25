f887bd2b390719194f7cb22acecd64154078a040
Kyuofox <kyuofox@gmail.com>
2026-06-25 22:53:24 +0800

    marble: Import compatibility_matrix from LA.VENDOR.1.0.r1-29700-WAIPIO.QSSI16.0
    Change-Id: I83f310d53c946a3db573ebb49dd7d7137685e966

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,6 +115,9 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/init/recovery.fstab
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_F2FS := true
 
+# VINTF
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
